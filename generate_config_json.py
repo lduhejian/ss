091116ceb152d4df361e8678f1008e8bc795b732
hejian <1217036457@qq.com>
2018-10-21 23:24:54 +0800

    更新数据后，保留上次选择的服务器

--- generate_config_json.py
+++ generate_config_json.py
@@ -1,21 +1,32 @@
 import json
 import uuid
 
+try:
+    with open('last-gui-config.json') as f1:
+        gui_config = json.load(f1)
+        LastActiveServerProfileId = gui_config['ActiveServerProfileId']
+except:
+    LastActiveServerProfileId = "not find"
+useLast = False
 with open('data.json') as f:
     accounts = json.load(f)
     result = "{\"LaunchAtLogin\": false,\"ShadowsocksRunningMode\": \"auto\",\"ServerProfiles\": ["
-    ActiveServerProfileId = ""
+    ActiveServerProfileId = LastActiveServerProfileId
     for account in accounts:
         ip = account['ip']
         port = account['port']
         password = account['password']
         encrypt = account['encrypt']
-        ServerProfileId = str(uuid.uuid1()).upper()
+        ServerProfileId = account['ip']+'-'+account['port']
         ActiveServerProfileId = ServerProfileId
+        if ServerProfileId == LastActiveServerProfileId:
+            useLast = True
         item = "{ \"Id\": \"" + ServerProfileId + "\", \"Method\": \"" + encrypt + "\", \"Plugin\": \"\",\"PluginOptions\": \"\",\"Password\": \"" + password + "\", \"Remark\": \"" + ip + "\", \"ServerHost\": \"" + ip + "\", \"ServerPort\": " + port + " },"
         # print(item)
         result = result + item
     if  result[-1] == ",":
         result = result[:-1]
+    if  useLast == True:
+        ActiveServerProfileId = LastActiveServerProfileId
     result = result + "],\"ActiveServerProfileId\": \"" + ActiveServerProfileId + "\",\"LocalSocks5.ListenAddress.Old\": \"127.0.0.1\",\"LocalSocks5.ListenPort.Old\": 1080,\"ShadowsocksOn\": true,\"NSNavPanelExpandedSizeForSaveMode\": \"{712, 521}\",\"NSNavLastRootDirectory\": \"~\/Desktop\",\"LocalSocks5.ListenPort\": 1080,\"NSNavPanelExpandedSizeForOpenMode\": \"{712, 448}\",\"LocalSocks5.EnableVerboseMode\": true}"
     print(result)
--- ss.sh
+++ ss.sh
@@ -3,10 +3,16 @@
 # 获取服务器配置列表
 curl -x socks5h://localhost:1080 "https://do.ishadowx.net/" -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92'  -H 'Accept-Language: zh-cn'   > html
 /usr/local/bin/python parse_html_to_array.py > data.json
-/usr/local/bin/python generate_config_json.py > gui-config.json
 
 if [ -s data.json ]
 then
+    # 首先将现有的配置导出
+    defaults export com.qiuyuzhou.ShadowsocksX-NG last-gui-config.plist
+    # 将 plist 二进制转成可读的 json 文件
+    plutil -convert json -o last-gui-config.json last-gui-config.plist
+    # 生成新的 gui-config.json
+    /usr/local/bin/python generate_config_json.py > gui-config.json
+
     # 将服务器配置列表导入 ShadowsocksX-NG
     plutil -convert xml1 gui-config.json -o gui-config.plist
     defaults import com.qiuyuzhou.ShadowsocksX-NG gui-config.plist
