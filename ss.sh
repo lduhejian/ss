c4a667410898e6ce6ec7137b6be7ec735aaff3c3
hejian <1217036457@qq.com>
2018-10-21 21:34:47 +0800

    修复 crontab 执行脚本时 data.json 为空的问题

--- ss.sh
+++ ss.sh
@@ -1,18 +1,17 @@
 #! /usr/bin/bash
 
-cd ~/ss
 # 获取服务器配置列表
 curl -x socks5h://localhost:1080 "https://do.ishadowx.net/" -H 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92'  -H 'Accept-Language: zh-cn'   > html
-python parse_html_to_array.py > data.json
-python generate_config_json.py > gui-config.json
+/usr/local/bin/python parse_html_to_array.py > data.json
+/usr/local/bin/python generate_config_json.py > gui-config.json
 
 # 将服务器配置列表导入 ShadowsocksX-NG
-plutil -convert xml1 gui-config.json -o gui-config.plist
-defaults import com.qiuyuzhou.ShadowsocksX-NG gui-config.plist
+# plutil -convert xml1 gui-config.json -o gui-config.plist
+# defaults import com.qiuyuzhou.ShadowsocksX-NG gui-config.plist
 
 # 重启 ShadowsocksX-NG
-osascript -e 'quit app "ShadowsocksX-NG"'
-sleep 1
-open -a shadowsocksx-ng
-sleep 1
+# osascript -e 'quit app "ShadowsocksX-NG"'
+# sleep 1
+# open -a shadowsocksx-ng
+# sleep 1
 osascript -e 'display notification "更新了 ss 账号"'
